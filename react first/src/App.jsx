53e46da4d9d7a38c658f102867943e48024adbb4
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-05-09 20:29:01 +0700

    fist folder react

---
+++ react first/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+
+function App() {
+  return(
+    <>
+    <Footer/>
+    <Header/>
+    <Food/>
+    </>
+  );
+}
+
+export default App
